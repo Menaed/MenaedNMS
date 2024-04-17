6f5827ec1b4b80a03bae6d74cefa34beafb2a27f
fcrubv <m18210617056@163.com>
2024-04-18 06:16:30 +0800

    增加Lua脚本

---
+++ Menaed.lua
@@ -0,0 +1,25 @@
+NMS_MOD_DEFINITION_CONTAINER = {
+    ["MOD_FILENAME"] = "MenaedHead.pak",
+    ["MOD_AUTHOR"] = "fcrubv",
+    ["LUA_AUTHOR"] = "fcrubv",
+    ["NMS_VERSION"] = "4.64",
+    ["MOD_DESCRIPTION"] = "Example For Mod Making Tutorial", --以上五条简称为一级词条 此注释来自AltF4
+    ["MODIFICATIONS"] = {
+    {
+    ["MBIN_CHANGE_TABLE"] = { --以上两条简称为二级词条
+    {
+    ["MBIN_FILE_SOURCE"] = "",
+    ["EXML_CHANGE_TABLE"] = { --以上两条简称为三级词条
+    { --以下简称为四级词条
+    ["SPECIAL_KEY_WORDS"] = {},
+    ["VALUE_CHANGE_TABLE"] = {
+    {},
+    }
+    },
+    }
+    },
+    },
+    },
+    }
+   }
+   
