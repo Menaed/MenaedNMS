6b98707787ed9a5212dafd0ddd11a7e187c6aee1
fcrubv <m18210617056@163.com>
2024-04-21 23:06:20 +0800

    增加源文件

--- Menaed.lua
+++ Menaed.lua
@@ -23,3 +23,34 @@ NMS_MOD_DEFINITION_CONTAINER = {
     }
    }
    
+
+
+
+   </Property>
+   <Property name="Children">
+     <Property value="TkSceneNodeData.xml">
+       <Property name="Name" value="RefHead" />
+       <Property name="NameHash" value="0" />
+       <Property name="Type" value="REFERENCE" />
+       <Property name="Transform" value="TkTransformData.xml">
+         <Property name="TransX" value="0" />
+         --X:UpDown
+         <Property name="TransY" value="0.1001" />
+         --Y:LR
+         <Property name="TransZ" value="0.00000" />
+         <Property name="RotX" value="0" />
+         <Property name="RotY" value="0" />
+         <Property name="RotZ" value="-90" />
+         <Property name="ScaleX" value="1.000010" />
+         <Property name="ScaleY" value="1.000010" />
+         <Property name="ScaleZ" value="1.000010" />
+       </Property>
+       <Property name="Attributes">
+         <Property value="TkSceneNodeAttributeData.xml">
+           <Property name="Name" value="SCENEGRAPH" />
+           <Property name="AltID" value="" />
+           <Property name="Value" value="CUSTOMMODELS\MENAED\SCENE.SCENE.MBIN" />
+         </Property>
+       </Property>
+       <Property name="Children" />
+     </Property>
